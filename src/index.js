5cccdc33c1bd4f9c9523e2ab4c89cd452fdf82b3
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-12-04 10:10:39 +0100

    moved store

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './state/store/index'
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore, compose, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './state/reducers/index';
 
-const store = createStore(
-    rootReducer,
-    {},
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
-    ),   
-)
+// const store = createStore(
+//     rootReducer,
+//     {},
+//     compose(
+//       applyMiddleware(thunk),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+//     ),   
+// )
 
 ReactDOM.render(
     <Router>
